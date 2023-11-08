7faf54488cf4b8e3c12441894bee55e6903a12e7
Abdul Waris <abdulwarisaw532@gmail.com>
2023-11-07 20:42:02 -0500

    Phase 3 progress

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -9,8 +9,30 @@ export function getDogNumberTwo() {
 }
 
 export function postNewDog() {
+    let url = "/dogs"
+        let headers =  {
+            "Content-Type": "application/x-www-form-urlencoded"
+        };
+
+      const body = new URLSearchParams({
+        name: "Tummy Rub",
+        age: "7 months"
+       });
+
+       const options = {
+        method: "POST",
+
+       }
+       return fetch (url, options)
+      
+    }
+ // return fetch(url, {
+    //     method: "POST",
+    const options = {
+
+    }
     
-}
+
 
 export function postNewDogV2(name, age) {
      // Your code here
